e9a8bf3744400aad3941afe24aebd0a41d08e506
Rochisha Agarwal <rochisha.agarwal2302@gmail.com>
2024-08-15 14:20:32 +0530

    change default dtype with backend

--- src/qutip_jax/settings.py
+++ src/qutip_jax/settings.py
@@ -1,7 +1,14 @@
 import jax.numpy as jnp
+import numpy as np
 from qutip import settings
 
 __all__ = ["use_jax_backend"]
 
 def use_jax_backend():
     settings.core['numpy_backend'] = jnp
+    settings.core["default_dtype"] = "jax"
+
+def use_numpy_backend():
+    settings.core['numpy_backend'] = np
+    settings.core["default_dtype"] = None
+
